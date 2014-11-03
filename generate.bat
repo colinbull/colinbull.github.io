4ce4fdda9f56395c0ba83e23d8722eb0674e5324
Colin <colinbul@googlemail.com>
2014-11-03 13:54:21 +0000

    Adding post template

--- generate.bat
+++ generate.bat
@@ -1,5 +1,5 @@
 @echo off
-call packages\FSharp.Formatting.CommandTool\tools\fsformatting.exe literate --processDirectory --inputDirectory  "code" --outputDirectory "_posts"
+call packages\FSharp.Formatting.CommandTool\tools\fsformatting.exe literate --processDirectory --templateFile "code/post_template.html" --lineNumbers false --inputDirectory  "code" --outputDirectory "_posts"
 
 git add .
 git commit -a -m %1
