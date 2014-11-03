36370fc8bdc711e8007f3d3145df6c64367dc07a
Colin <colinbul@googlemail.com>
2014-11-03 17:45:00 +0000

    Hopefully generating with titles

--- generate.bat
+++ generate.bat
@@ -1,5 +1,7 @@
 @echo off
-call packages\FSharp.Formatting.CommandTool\tools\fsformatting.exe literate --processDirectory --lineNumbers false --inputDirectory  "code" --outputDirectory "_posts"
+REM call packages\FSharp.Formatting.CommandTool\tools\fsformatting.exe literate --processDirectory --lineNumbers false --inputDirectory  "code" --outputDirectory "_posts"
+
+call "C:\Program Files (x86)\Microsoft SDKs\F#\3.1\Framework\v4.0\fsi.exe" generate.fsx
 
 git add .
 git commit -a -m %1
