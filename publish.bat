996f64983255dd7aa139da7af6cb994130284c00
Colin <colinbul@googlemail.com>
2014-11-04 10:17:27 +0000

    duhhhrrr

--- publish.bat
+++ publish.bat
@@ -3,6 +3,6 @@
 call .paket\paket restore
 call tools\fsformatting.exe literate --processDirectory --lineNumbers false --inputDirectory  "code" --outputDirectory "_posts"
 
-git add .
+git add --all .
 git commit -a -m %1
 git push
