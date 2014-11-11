a65aa27e1275fd66004b770b3640104e57d70b7c
Colin <colinbul@googlemail.com>
2014-11-11 21:09:26 +0000

    Draft of introducing cricket

--- code/2014-11-06-Introducing-Cricket.fsx
+++ code/2014-11-06-Introducing-Cricket.fsx
@@ -9,10 +9,10 @@ title: Introducting Cricket.
 #r "FsPickler/FsPickler.dll"
 #r "Cricket/Cricket.dll"
 
-type 
+open Cricket
 
 (**
-Over the last few years, I have been quite taken by the actor model of computing. Although not a silver bullet it does tend to make concurrent programming many orders of magnitude easier to reason about. If you have never heard of Actors then an actor as defined by [wikipedia](http://en.wikipedia.org/wiki/Actor_model) is as follows
+Over the last few years, I have been quite taken by the actor model of computing. Although not a silver bullet it does tend to make concurrent programming orders of magnitude easier to reason about. If you have never heard of Actors then an actor as defined by [wikipedia](http://en.wikipedia.org/wiki/Actor_model) is as follows
 
 > The Actor model adopts the philosophy that everything is an actor. This is similar to the everyt>hing is an object philosophy used by some object-oriented programming languages, but differs in t>hat object-oriented software is typically executed sequentially, while the Actor model is inheren>tly concurrent.
 >
@@ -26,14 +26,22 @@ I also encourage you to look at [Erlang/Elixir](http://www.erlang.org), [Akka](h
 
 ##Introducing Cricket
     
-[Cricket](http://github.com/fsprojects/Cricket) (formally FSharp.Actor)  is yet another actor framework. Built entirely in F#, Cricket is a lightweight alternative to Akka et. al. To this end it is not as feature rich as these out of the box, but all of the core requirements like location transpancy, remoting, supervisors, metrics and tracing. Other things like failure detection and clustering are in the pipeline it is just a question of time. Currently the focus of the project is to get things running smoothly on both .NET CLR and Mono Runtimes. Anyway enough of that, to the code.
+[Cricket](http://github.com/fsprojects/Cricket), formally FSharp.Actor, is yet another actor framework. Built entirely in F#, Cricket is a lightweight alternative to Akka et. al. To this end it is not as feature rich as these out of the box, but all of the core requirements like location transpancy, remoting, supervisors, metrics and tracing. Other things like failure detection and clustering are in the pipeline it is just a question of time.
+
+Some key links for Cricket:
+
+* [Cricket Home Page](http://fsprojects.github.io/Cricket)
+* [Cricket Source code](http://github.com/fsprojects/Cricket) on GitHub
+* [Cricket NuGet package](http://www.nuget.org/packages/Cricket)
+
+The nuget package, contains a single library `Cricket.dll` which contains the Cricket API. The following example, creates a `echo` actor using cricket.   
 *)
 
 ActorHost.Start()
 
-let sayHello =
+let echo =
     actor {
-        name "hello"
+        name "echo"
         body (
             let rec loop() = messageHandler {
                 let! msg = Message.receive()
@@ -43,8 +51,7 @@ let sayHello =
             loop())
     } |> Actor.spawn
 
-
-sayHello <-- "Hello"
+echo <-- "Hello, from Cricket"
 
 (**
 
