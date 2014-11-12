1e5dcc50387d75dc86e87d05074f2b22a6d8f376
Colin <colinbul@googlemail.com>
2014-11-12 06:36:32 +0000

    Updating generator script

---
+++ generate.fsx
@@ -0,0 +1,12 @@
+﻿#I "tools"
+#r "FSharp.Markdown.dll"
+#r "FSharp.CodeFormat.dll"
+#r "FSharp.Literate.dll"
+open FSharp.Literate
+open System.IO
+
+let codeDirectory = __SOURCE_DIRECTORY__ + "/code"
+let postsDirectory = __SOURCE_DIRECTORY__ + "/_posts"
+let fsiEval = FsiEvaluator()
+
+Literate.ProcessDirectory(codeDirectory, outputDirectory = postsDirectory, format = OutputKind.Html, lineNumbers = false, fsiEvaluator = fsiEval, generateAnchors = true)
