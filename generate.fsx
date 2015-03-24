77e0d87c73ad07009d47595ad399c96fd003d8b1
Colin <colinbul@googlemail.com>
2015-03-24 15:27:16 +0000

    Hiding chart creation

--- generate.fsx
+++ generate.fsx
@@ -8,6 +8,6 @@ open System.IO
 
 let codeDirectory = __SOURCE_DIRECTORY__ + "/code"
 let postsDirectory = __SOURCE_DIRECTORY__ + "/_posts"
-let fsiEval = Formatters.createFsiEvaluator "public/blog_imgs" "#.####"
+let fsiEval = Formatters.createFsiEvaluator "../public/blog_imgs" "#.####"
 
 Literate.ProcessDirectory(codeDirectory, outputDirectory = postsDirectory, format = OutputKind.Html, lineNumbers = true, fsiEvaluator = fsiEval, generateAnchors = true)
